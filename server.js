b3e7e44b6c8f75cdfdaae65240fb073922d2f808
GoldbeardSea <timbusch1@gmail.com>
2018-05-29 15:30:57 -0700

    server runs and can run queries to the mealsDB when testing in the addressbar by strMeal=input

--- server.js
+++ server.js
@@ -5,13 +5,6 @@ const cors = require('cors');
 const pg = require('pg');
 const superagent = require('superagent');
 
-'use strict'
-
-const express = require('express');
-const cors = require('cors');
-const pg = require('pg');
-const superagent = require('superagent');
-
 const app = express();
 const PORT = process.env.PORT;
 
@@ -19,12 +12,13 @@ const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 client.on('error', err => console.error(err));
 
+app.get('/', (req, res) => res.send('Testing 1, 2, 3, 4, 5, eat'));
 // TO DO: ensure that this is linked to routes and pages
 // TO DO: write out different ways to search the API using different titles, ingredients, and
 app.get('/api/json/recipes/ingredients', (req, res) => {
   let url = 'https://www.themealdb.com/api/json/v1/1/filter.php'
   let query = '';
-  // look into this more not sure it is correct. 
+  // look into this more not sure it is correct.
   if(req.query.strMeal) query += `${req.query.strMeal}`;
   superagent.get(url)
     .query({'i': query})
@@ -34,27 +28,17 @@ app.get('/api/json/recipes/ingredients', (req, res) => {
 app.get('/api/json/recipes/name', (req, res) => {
   let url = 'https://www.themealdb.com/api/json/v1/1/search.php'
   let query = '';
-  // look into this more not sure it is correct. 
+  // look into this more not sure it is correct.
   if(req.query.name) query += `${req.query.name}`;
   superagent.get(url)
     .query({'s': query})
     .then(response => res.send(response.body.meals));
 });
 
-app.get('/api/json/recipes/ingredients', (req, res) => {
-  let url = 'https://www.themealdb.com/api/json/v1/1/filter.php'
-  let query = '';
-  // look into this more not sure it is correct. 
-  if(req.query.strMeal) query += `${req.query.strMeal}`;
-  superagent.get(url)
-    .query({'i': query})
-    .then(response => res.send(response.body.meals));
-});
-
 app.get('/api/json/recipes/categories', (req, res) => {
   let url = 'https://www.themealdb.com/api/json/v1/1/filter.php'
   let query = '';
-  // look into this more not sure it is correct. 
+  // look into this more not sure it is correct.
   if(req.query.strMeal) query += `${req.query.strMeal}`;
   superagent.get(url)
     .query({'c': query})
@@ -64,12 +48,13 @@ app.get('/api/json/recipes/categories', (req, res) => {
 app.get('/api/json/recipes/area', (req, res) => {
   let url = 'https://www.themealdb.com/api/json/v1/1/filter.php'
   let query = '';
-  // look into this more not sure it is correct. 
+  // look into this more not sure it is correct.
   if(req.query.strMeal) query += `${req.query.strMeal}`;
   superagent.get(url)
     .query({'a': query})
     .then(response => res.send(response.body.meals));
 });
+
 
 ///// DATABASE LOADER /////
 ///////////////////////////
